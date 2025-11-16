71cf595f0d4d9f0b69b4f22ec478d68afe3be847
veinsnwires <matias.reyeso@usm.cl>
2025-11-16 14:36:48 -0300

    Configuración Firebase inicial, servicios de Google

---
+++ lib/pages/home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key, required String title});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("HOLAA"));
+  }
+}
